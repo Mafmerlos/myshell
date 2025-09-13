d21e9fcfd1f78079ca6a6a60e6f99199a139e978
Mafmerlos <matheusmerlos02@gmail.com>
2025-09-13 10:30:55 -0300

    Implementa a quebra do comando em argumentos

--- myshell.c
+++ myshell.c
@@ -3,33 +3,45 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define DELIMITADORES " \t\r\n\a"
 
-int main(){
-char total_linhas[256];
-char *saida;
+int main() {
+    char total_linhas[256];
+    char *args[64];
+    int i;
 
-while(true){
-	printf("myshell> ");
+    while (true) {
+        printf("myshell> ");
 
-	 if(fgets(total_linhas,sizeof(total_linhas),stdin)==NULL){
-	 printf("\n");
-	 break;
-      }
+        if (fgets(total_linhas, sizeof(total_linhas), stdin) == NULL) {
+            printf("\n");
+            break;
+        }
 
-	saida = (strtok(total_linhas," \n\r\t\a"));
+        i = 0;
+        args[i] = strtok(total_linhas, DELIMITADORES);
 
-	if(saida == NULL){
-	continue;
-	}
+        if (args[0] == NULL) {
+            continue;
+        }
 
-	if(strcmp(saida,"exit")==0){
-		printf("Saindo shell... Volte sempre!\n");
-		break;
-}
-		printf("Comando digitado ainda está na parte de desenvolvimento...\n");
+        while (args[i] != NULL) {
+            i++;
+            args[i] = strtok(NULL, DELIMITADORES);
+        }
 
-}
+        if (strcmp(args[0], "exit") == 0) {
+            printf("Saindo shell... Volte sempre!\n");
+            break;
+        }
 
-printf("Meu shell foi encerrado...");
- return 0;
+        printf("Análise do comando:\n");
+        for (int j = 0; j < i; j++) {
+            printf("  Argumento[%d]: %s\n", j, args[j]);
+        }
+        printf("--- Fim da análise ---\n");
+    }
+
+    printf("Meu shell foi encerrado...\n");
+    return 0;
 }
